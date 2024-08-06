d358c1fd0d2a01390e4c7d4bac49e0d4709c1a11
Harashveer <harashveersinghjaura@gmail.com>
2024-08-06 16:33:17 -0400

    Add files via upload

---
+++ project/scripts.js
@@ -0,0 +1,7 @@
+// Add JavaScript functionality as needed
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("JavaScript is loaded");
+});
+document.getElementById("addToCartBtn").addEventListener("click", function () {
+  alert("Laser Active has been added to your cart!");
+});
